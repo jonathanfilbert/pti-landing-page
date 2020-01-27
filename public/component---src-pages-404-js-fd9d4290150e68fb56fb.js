5cffe79e9063a82431105f06f8e04baae7e4ca86
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-01-27 11:10:58 +0700

    addd built

---
+++ public/component---src-pages-404-js-fd9d4290150e68fb56fb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,t,e){"use strict";e.r(t);var c=e("q1tI"),u=e("Wbzz");t.default=function(){return Object(c.useEffect)((function(){Object(u.navigate)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fd9d4290150e68fb56fb.js.map
